8a5b8d3fd13c556599e670f792c613daa2fd3bd8
Bui Quang Huy <mr.buiquanghuy@gmail.com>
2019-07-17 07:05:53 +0700

    make:auth commit

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::post('auth/login', 'Api\UsersController@login');
+Route::post('auth/login', 'Api\UsersController@login');
 
 Route::group(['middleware' => 'jwt.auth'], function () {
     // User
