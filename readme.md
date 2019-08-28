9bac0457fcef45179ea9065b04ab148b680aa41f
Bui Quang Huy <mr.buiquanghuy@gmail.com>
2019-08-28 23:22:22 +0700

    remove file readme

--- readme.md
+++
@@ -1,8 +0,0 @@
-Guideline
-1. Clone project
-2. Copy file .env.example -> change file name to .env
-3. Config database connection information
-4. Run command 
-    composer update
-5. Run command 
-    php artisan key:generate 
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+Guideline
+1. Clone project
+2. Copy file .env.example -> change file name to .env
+3. Config database connection information
+4. Run command 
+    composer update
+5. Run command 
+    php artisan key:generate 
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
